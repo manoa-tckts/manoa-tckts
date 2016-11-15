155c3d6cdf66d88b99223257329a78f7e5ffac90
dumlaoj <jordanobena@gmail.com>
2016-11-15 00:45:03 -1000

    Update XMLs

---
+++ app/imports/api/events/list-of-events.js
@@ -0,0 +1,44 @@
+import { Mongo } from 'meteor/mongo';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+/* eslint-disable object-shorthand */
+
+export const ListOfEvents = new Mongo.Collection('ListOfEvents');
+
+/**
+ * Create the schema for Stuff
+ */
+export const EventsSchema = new SimpleSchema({
+  event: {
+    label: 'event',
+    type: String,
+    optional: false,
+    max: 2000,
+  },
+  date: {
+    label: 'date',
+    type: String,
+    optional: false,
+    max: 20,
+  },
+  location: {
+    label: 'location',
+    type: String,
+    optional: false,
+    max: 2000,
+  },
+  price: {
+    label: 'price',
+    type: String,
+    optional: false,
+    max: 200,
+  },
+  negotiable: {
+    label: 'negotiable',
+    type: String,
+    optional: true,
+    max: 200,
+  },
+});
+
+ListOfEvents.attachSchema(EventsSchema);
