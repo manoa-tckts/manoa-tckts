a0b424b3ff954a0f9eb72e3914f7574caa2a5858
Brent Nunogawa <nunogawa@hawaii.edu>
2016-11-14 22:24:25 -1000

    finished
    finished profile and edit profile pages

--- app/imports/ui/pages/profile-page.js
+++ app/imports/ui/pages/profile-page.js
@@ -11,6 +11,27 @@ Template.Profile_Page.helpers({
   user: function user() {
     return Meteor.user() ? Meteor.user().profile.name : 'No logged in user';
   },
+  first: function() {
+    return Meteor.user().profile.first;
+  },
+  last: function() {
+    return Meteor.user().profile.last;
+  },
+  telephone: function() {
+    return Meteor.user().profile.telephone;
+  },
+  email: function() {
+    return Meteor.user().profile.email;
+  },
+  motto: function() {
+    return Meteor.user().profile.motto;
+  },
+  miscellaneous: function() {
+    return Meteor.user().profile.miscellaneous;
+  },
+  picture: function() {
+    return Meteor.user().profile.picture;
+  },
 });
 
 Template.Profile_Page.events({
