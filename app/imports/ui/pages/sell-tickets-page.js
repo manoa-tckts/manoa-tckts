0485e03b1aa9c298afbeb968831abdfd2b8d5c10
brandon-chun <bchun9@hawaii.edu>
2016-11-22 20:35:19 -1000

    created separate sellers page and working on sell tickets page

--- app/imports/ui/pages/sell-tickets-page.js
+++ app/imports/ui/pages/sell-tickets-page.js
@@ -3,44 +3,42 @@
  * Edited by Brandon on 11/14/16.
  */
 
-
-import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Template } from 'meteor/templating';
+import { FlowRouter } from 'meteor/kadira:flow-router';
 import { _ } from 'meteor/underscore';
-import { PeopleBuyingTickets } from '../../api/people-buying-tickets/list-of-people-buying-tickets.js';
+import { Ticket, TicketSchema } from '../../api/schema/schemas.js';
+import { Meteor } from 'meteor/meteor';
 
 
 Template.Sell_Tickets_Page.onCreated(function onCreated() {
-  this.autorun(() => {
-    this.subscribe('PeopleBuyingTickets');
-  });
 });
 
 Template.Sell_Tickets_Page.helpers({
-  pbtList() {
-    return PeopleBuyingTickets.find();
+  ticketList() {
+    return Ticket.find();
   },
 });
 
-/*Template.Sell_Tickets_Page.events({
-  'submit .ticket-data-form'(event, instance) {
+Template.Sell_Tickets_Page.events({
+  'submit form'(event, instance) {
     event.preventDefault();
     // Get name (text field)
-    const newevent = event.target.event.value;
+    const eventName = event.target.eventName.value;
     const date = event.target.date.value;
-    const location = event.target.location.value;
+    const status = 'sell';
     const price = event.target.price.value;
     const negotiable = event.target.negotiable.value;
+    const owner = Meteor.userId();
 
-    const newTicket = { newevent, date, location, price, negotiable };
+    const newTicket = { eventName, status, date, price, negotiable, owner };
     // Clear out any old validation errors.
     instance.context.resetValidation();
     // Invoke clean so that newStudentData reflects what will be inserted.
-    PBTSchema.clean(newTicket);
+    TicketSchema.clean(newTicket);
     // Determine validity.
     instance.context.validate(newTicket);
     if (instance.context.isValid()) {
-      PeopleBuyingTickets.insert(newTicket);
+      Ticket.insert(newTicket);
       FlowRouter.go('Profile_Page');
     } else {
       FlowRouter.go('Sell_Tickets_Page');
@@ -48,4 +46,3 @@ Template.Sell_Tickets_Page.helpers({
   },
 
 });
-*/
