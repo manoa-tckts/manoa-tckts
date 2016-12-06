bf7e45eefe00abe59c481c2306afcf36e7541516
Brent Nunogawa <nunogawa@hawaii.edu>
2016-12-05 21:28:18 -1000

    changed again for the 4th time I think

---
+++ app/imports/startup/server/members.js
@@ -0,0 +1,3 @@
+import {Members} from '../../api/schema/members.js';
+import {_} from 'meteor/underscore';
+import {Meteor} from 'meteor/meteor';
--- app/imports/ui/pages/profile-page.js
+++ app/imports/ui/pages/profile-page.js
@@ -1,11 +1,15 @@
 /**
  * Created by Jordan on 10/27/16.
  */
+
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { Members } from '../../api/schema/members.js';
+import { PeopleBuyingTickets } from '../../api/people-buying-tickets/list-of-people-buying-tickets.js';
 
 /* eslint-disable object-shorthand */
 
+/*
 Template.Profile_Page.helpers({
 
   user: function user() {
@@ -31,6 +35,64 @@ Template.Profile_Page.helpers({
   },
   picture: function() {
     return Meteor.user().profile.picture;
+  },
+});
+
+Template.Profile_Page.events({
+
+
+});
+*/
+
+Template.Profile_Page.helpers({
+
+  user: function user() {
+    return Meteor.user() ? Meteor.user().profile.name : 'No logged in user';
+  },
+  first: function() {
+    if(Members.find({uid: Meteor.userId()}, {limit: 1}).count() <= 0){
+      return('Please edit your profile');
+    }
+    else {return Members.findOne({'uid':Meteor.userId()}).first;}
+  },
+  last: function() {
+    if(Members.find({uid: Meteor.userId()}, {limit: 1}).count() <= 0){
+      return('Please edit your profile');
+    }
+    else {return Members.findOne({'uid':Meteor.userId()}).last;}
+  },
+  telephone: function() {
+    if(Members.find({uid: Meteor.userId()}, {limit: 1}).count() <= 0){
+      return('Please edit your profile');
+    }
+    else {return Members.findOne({'uid':Meteor.userId()}).phone;}
+  },
+  email: function() {
+    if(Members.find({uid: Meteor.userId()}, {limit: 1}).count() <= 0){
+      return('Please edit your profile');
+    }
+    else {return Members.findOne({'uid':Meteor.userId()}).email;}
+  },
+  motto: function() {
+    if(Members.find({uid: Meteor.userId()}, {limit: 1}).count() <= 0){
+      return('Please edit your profile');
+    }
+    else {return Members.findOne({'uid':Meteor.userId()}).motto;}
+  },
+  miscellaneous: function() {
+    if(Members.find({uid: Meteor.userId()}, {limit: 1}).count() <= 0){
+      return('Please edit your profile');
+    }
+    else {return Members.findOne({'uid':Meteor.userId()}).miscellaneous;}
+  },
+  picture: function() {
+    if(Members.find({uid: Meteor.userId()}, {limit: 1}).count() <= 0){
+      return('/images/matthew.png');
+    }
+    else{return Members.findOne({'uid':Meteor.userId()}).picture;}
+  },
+  ticketList() {
+    return PeopleBuyingTickets.find();
   },
 });
 
